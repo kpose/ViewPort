900db9c48c2fd6f0ec3f6027f1666cc112ba3e50
!Kpose <ganihujude@gmail.com>
2023-02-14 15:10:45 +0100

    Add custom tweet row view with static data

--- ViewPort/Core/Components/Tweets/TweetRowView.swift
+++ ViewPort/Core/Components/Tweets/TweetRowView.swift
@@ -9,8 +9,68 @@ import SwiftUI
 
 struct TweetRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack(alignment: .top, spacing: 12) {
+                Circle()
+                    .frame(width: 60, height: 60)
+                    .foregroundColor(Color.blue)
+                    
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("Kpose Richard")
+                            .font(.subheadline).bold()
+                        Text("@Kpooose")
+                            .foregroundColor(Color.gray)
+                            .font(.caption)
+                        Text(". 2w ago")
+                            .foregroundColor(Color.gray)
+                            .font(.caption)
+                    }
+                    
+                    Text("This is the tweet body, how do you respond?").font(.subheadline).multilineTextAlignment(.leading)
+                }
+                
+            }
+            //action buttons
+            HStack {
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "bubble.left").font(.subheadline)
+                }
+                Spacer()
+                
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "arrow.2.squarepath").font(.subheadline)
+                }
+                Spacer()
+
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "heart").font(.subheadline)
+                }
+                Spacer()
+
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "bookmark").font(.subheadline)
+                }
+                
+
+            }
+            .padding()
+            .foregroundColor(.gray)
+            
+            Divider()
+        }
+        .padding()
+        
     }
+    
 }
 
 struct TweetRowView_Previews: PreviewProvider {
