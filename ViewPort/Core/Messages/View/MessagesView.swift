253899dd5ad9281b8ed71031b20237f05107c51f
!Kpose <ganihujude@gmail.com>
2023-02-14 15:27:55 +0100

    add new screen view to main tab navigation view

---
+++ ViewPort/Core/Messages/View/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  ViewPort
+//
+//  Created by Jude Ganihu on 14/02/2023.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
---
+++ ViewPort/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  ViewPort
+//
+//  Created by Jude Ganihu on 14/02/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
