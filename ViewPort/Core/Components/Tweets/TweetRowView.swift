85f2c5427d7361c4b9d6fb137de6152e17360682
!Kpose <ganihujude@gmail.com>
2023-02-14 15:10:26 +0100

    Add custom tweet row view with static data

---
+++ ViewPort/Core/Components/Tweets/TweetRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetRowView.swift
+//  ViewPort
+//
+//  Created by Jude Ganihu on 14/02/2023.
+//
+
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
