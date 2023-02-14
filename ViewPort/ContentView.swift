33bcb3cd67d7bc8f480b3b2f25e6d60e04ee7501
!Kpose <ganihujude@gmail.com>
2023-02-14 11:19:31 +0100

    Initial Commit

---
+++ ViewPort/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewPort
+//
+//  Created by Jude Ganihu on 14/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewPort/ViewPortApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewPortApp.swift
+//  ViewPort
+//
+//  Created by Jude Ganihu on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewPortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
