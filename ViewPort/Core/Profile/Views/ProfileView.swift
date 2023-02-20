b2fdaeac2e0e0c52bbfb102215c77be30a0ae685
!Kpose <ganihujude@gmail.com>
2023-02-20 16:19:21 +0100

    create profile view with dumy data

--- ViewPort/Core/Profile/Views/ProfileView.swift
+++ ViewPort/Core/Profile/Views/ProfileView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var selectedFilter: TweetFilterViewModel = .tweets
+
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             headerView
             actionButtons
+            userInfoDetails
+            tweetFilterBar
+            tweetsView
+            
             
             
             Spacer()
@@ -73,5 +80,90 @@ extension ProfileView {
             }
         }
         .padding(.trailing)
+    }
+    
+    var userInfoDetails: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Text("Kpose Richard")
+                    .font(.title2).bold()
+                
+                Image(systemName: "checkmark.seal.fill")
+                    .foregroundColor(Color(.systemBlue))
+            }
+            Text("@kpoosee")
+                .foregroundColor(.gray)
+                .font(.subheadline)
+        
+            Text("Everything between the center")
+                .font(.subheadline)
+                .padding(.vertical)
+        
+            HStack(spacing: 20) {
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                    Text("Amsterdam, ND")
+                }
+                
+                HStack {
+                    Image(systemName: "link")
+                    Text("www.kpose.com")
+                }
+            
+            }.foregroundColor(.gray)
+                .font(.caption)
+            
+            HStack(spacing: 20) {
+                HStack(spacing:24) {
+                    Text("2").font(.subheadline).bold()
+                    Text("Following").foregroundColor(.gray)
+                }
+                
+                HStack {
+                    Text("2").font(.subheadline).bold()
+                    Text("Followers").foregroundColor(.gray).font(.caption)
+                    
+                }
+            }
+            .padding(.vertical)
+            
+        }.padding(.horizontal)
+    }
+    
+    var tweetFilterBar: some View {
+        HStack {
+            ForEach(TweetFilterViewModel.allCases, id: \.rawValue) { item in
+                VStack {
+                    Text(item.title)
+                        .fontWeight(selectedFilter == item ? .semibold : .regular)
+                        .foregroundColor(selectedFilter == item ? .black : .gray)
+                    if (selectedFilter == item) {
+                        Capsule()
+                            .foregroundColor(Color(.systemBlue))
+                            .frame(height: 3)
+                            
+                    } else {
+                        Capsule()
+                            .foregroundColor(Color(.clear))
+                            .frame(height: 3)
+                    }
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        self.selectedFilter = item
+                    }
+                }
+            }
+        }
+        .overlay(Divider().offset(x: 0, y: 16))
+    }
+    
+    var tweetsView: some View {
+        ScrollView {
+            ForEach(0 ... 9, id: \.self) { _ in
+                TweetRowView()
+                    .padding()
+            }
+        }
     }
 }
